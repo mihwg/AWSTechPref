47638203595410cf433c71504a7829eddefee81a
Michael Haller <michael.haller@sap.com>
2018-11-14 10:32:20 +0100

    Added preference put request
    Change-Id: I2d63062a9f0f9401c3a7b38dba487ec639114644

--- index.js
+++ index.js
@@ -2,6 +2,37 @@
 
 var fs = require('fs');
 var path = require('path');
+var https = require('https');
+
+const data = JSON.stringify({
+	"TechPref":"Serverless"
+})
+
+const options = {
+  hostname: 't6zzpojhqj.execute-api.eu-central-1.amazonaws.com',
+  port: 443,
+  path: '/prod/items/EDAF9D4C-25ED-4DBF-BF3D-EC6B1B7D8FF3',
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
